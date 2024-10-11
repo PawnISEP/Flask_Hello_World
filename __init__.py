e67946e3cbbd147daef6d11fb451af88d721512a
PawnISEP <mesoponions@gmail.com>
2024-10-11 13:50:40 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,3 +15,5 @@ def exercices():
                                                                                                                
 if __name__ == "__main__":
   app.run(debug=True)
+
+
